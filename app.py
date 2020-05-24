c2287b6fe2e64020df818035ff0dabaf1b3b1633
suongMai <suong05mt@gmail.com>
2020-05-24 23:53:58 +0700

    change web port

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ werkzeug.cached_property = werkzeug.utils.cached_property
 application = create_app()
 
 if __name__ == '__main__':
-    application.run(host="0.0.0.0", port=80,debug=True)
+    application.run(host="0.0.0.0", port=5000,debug=True)
     
