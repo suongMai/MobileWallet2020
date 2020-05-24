896bf1fd1bf783faead8130656bafd0e7e7d0907
suongMai <suong05mt@gmail.com>
2020-05-24 23:39:03 +0700

    edit docker file

--- mobilewallet/config.py
+++ mobilewallet/config.py
@@ -1,7 +1,10 @@
 import os
 
 #todo: push all creaentials to enviroment variable
-MONGO_URI = "mongodb+srv://wallet2020:wallet2020@cluster0-h78d3.gcp.mongodb.net/wallet2020?retryWrites=true&w=majority"\
+if os.environ.get('ENV') is not None:
+    MONGO_URI = "mongodb+srv://{}:{}@localhost:27017/{}".format(os.environ.get('MONGO_USER'),os.environ.get('MONGO_PASS'),os.environ.get('MONGO_DB'))
+else:
+    MONGO_URI = "mongodb+srv://wallet2020:wallet2020@cluster0-h78d3.gcp.mongodb.net/wallet2020?retryWrites=true&w=majority"\
 
 JWT_SECRET_KEY = "this-is-secrect"
 
