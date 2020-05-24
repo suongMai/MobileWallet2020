b3102db6af0b2bfb4d26570dc7ea4e35c99c6ca4
suongMai <suong05mt@gmail.com>
2020-05-24 23:46:16 +0700

    fix docker compose syntax

--- mobilewallet/config.py
+++ mobilewallet/config.py
@@ -2,7 +2,7 @@ import os
 
 #todo: push all creaentials to enviroment variable
 if os.environ.get('ENV') is not None:
-    MONGO_URI = "mongodb+srv://{}:{}@localhost:27017/{}".format(os.environ.get('MONGO_USER'),os.environ.get('MONGO_PASS'),os.environ.get('MONGO_DB'))
+    MONGO_URI = "mongodb+srv://{}:{}@{}:27017/{}".format(os.environ.get('MONGO_USER'),os.environ.get('MONGO_PASS'),os.environ.get('MONGODB_HOSTNAME'),os.environ.get('MONGO_DB'))
 else:
     MONGO_URI = "mongodb+srv://wallet2020:wallet2020@cluster0-h78d3.gcp.mongodb.net/wallet2020?retryWrites=true&w=majority"\
 
